78743efdef330d09bf5619267bd7b4000ad7c8f8
pepea23 <32633442+pepea23@users.noreply.github.com>
2019-02-15 18:11:46 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
         }),
         new BrowserSyncPlugin({
             host: process.env.IP || 'localhost',
-            port: process.env.PORT || 3000,
+            port: process.env.PORT || 3006,
             server: {
                 baseDir: ['./', './dev']
             }
